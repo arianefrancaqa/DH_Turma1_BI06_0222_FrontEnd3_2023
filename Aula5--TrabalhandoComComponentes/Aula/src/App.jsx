fe8794eb0bed6550ca1990900dc1dae0a1b51c73
arianefrancaqa <arianefrancaqa@gmail.com>
2023-05-16 23:27:07 -0300

    aula 4

--- Aula5--TrabalhandoComComponentes/Aula/src/App.jsx
+++ Aula5--TrabalhandoComComponentes/Aula/src/App.jsx
@@ -1,34 +1,27 @@
+import ItemComponent from "./components/ItemComponent";
 import ctdEstrutura from "./ctd_estrutura";
+import "./styles.css";
 
 function App() {
 
   return (
     <>
-      <h1>CTD - Jornada do Aluno</h1>
-
-      <div>
-        <h1>Bimestre {ctdEstrutura[0].bimestre} - Ano {ctdEstrutura[0].ano}</h1>
-        <h3>Disciplinas</h3>
-        <p>{ctdEstrutura[0].disciplinas[0]}</p>
-        <p>{ctdEstrutura[0].disciplinas[1]}</p>
-        <p>{ctdEstrutura[0].disciplinas[2]}</p>
+      <div className="container">
+        <h1 className="title">CTD - Jornada do Aluno</h1>
+        {
+          ctdEstrutura.map((item, index) => {
+            return (
+              <ItemComponent
+                key={index}
+                bimestre={item.bimestre}
+                ano={item.ano}
+                disciplinas={item.disciplinas}
+              />
+            )
+          })
+        }
       </div>
 
-      <div>
-        <h1>Bimestre {ctdEstrutura[1].bimestre} - Ano {ctdEstrutura[1].ano}</h1>
-        <h3>Disciplinas</h3>
-        <p>{ctdEstrutura[1].disciplinas[0]}</p>
-        <p>{ctdEstrutura[1].disciplinas[1]}</p>
-        <p>{ctdEstrutura[1].disciplinas[2]}</p>
-      </div>
-
-      <div>
-        <h1>Bimestre {ctdEstrutura[2].bimestre} - Ano {ctdEstrutura[2].ano}</h1>
-        <h3>Disciplinas</h3>
-        <p>{ctdEstrutura[2].disciplinas[0]}</p>
-        <p>{ctdEstrutura[2].disciplinas[1]}</p>
-        <p>{ctdEstrutura[2].disciplinas[2]}</p>
-      </div>
     </>
   )
 }
